5b8eab08c0878686b56f4c6ae5312b11631af1aa
MattSokol79 <maciej.sokol79@Gmail.com>
2020-11-03 19:04:09 +0000

    Loop and Age Tasks

--- task_2_loops.py
+++ task_2_loops.py
@@ -21,8 +21,14 @@ print(list_names)
 # Make a loop that iterated over each name in the above list_names and format it
 # into lowercase in a new variable called list_names_lower
 list_names_lower = []
-for i in list_names:
-    list_names_lower.append(i.lower())
-print(list_names_lower)
+for name in list_names:
+    list_names_lower.append(name.lower())
+print("The names in lowercase:", list_names_lower)
 
 # Iterate over the list of names to find if the number of letters is even or odd
+even_list = []
+for name in list_names_lower:
+    if len(name) % 2 == 0:
+        print(f"{name} contains an even number of letters")
+    else:
+        print(f"{name} contains an odd number of letters")
--- tast_1_user_age.py
+++ tast_1_user_age.py
@@ -1,27 +1,26 @@
 import datetime
+from datetime import date
 
-# Asks for users age and calculates year of birth
-age = int(input("Please input your age. "))
-name = input("Please input your full name. ").title()
-year_of_birth = datetime.datetime.now().year - age
-
-print(f"Omg {name} you are {age} years old so you were born in {year_of_birth}")
-
-birthday = input("Enter your date of birth in the format dd/mm/yyyy. ")
+# Ask user for details and calculate
+age = int(input("What is your age? "))
+name = input("What is your full name? ").title()
+birthday = input("What is your birthday?\nFormat: dd/mm/yyyy. ")
 bday = datetime.datetime.strptime(birthday, '%d/%m/%Y')
+birth_year = date.today().year - age
 
-# Fetches their age
-real_age = datetime.datetime.now() - bday
-real_age_hours = real_age * 24
+print(f"Omg {name}, you are {age} years old so you were born in {birth_year}.")
 
-print(real_age)
-print(f"In hours, you are in fact {real_age_hours} hours long")
-# print(f'Day: {bday.day}')
-# print(f'Month: {bday.month}')
-# print(f'Year: {bday.year}')
 
-# This only takes the month into account
-if datetime.datetime.now().month > bday.month:
-    print("Hope your birthday this year was amazing!! ")
+# Extension - determining whether the user has already celebrated their birthday
+today = date.today()
+months_left = bday.month - today.month
+days_left =  bday.day - today.day
+
+if bday.month - today.month < 0 and bday.day - today.day < 0:
+    print(f"{name}, hope you had a great birthday this year!")
 else:
-    print("Only a couple of months left until your birthday! ")
+    print(f"{name} you only have {days_left} days and {months_left} months left until your birthday")
+
+# Hours alive - difference between the current date and birthdate in seconds divided by 3600
+age_hours = (today - bday.date()).total_seconds() / 3600
+print(f"{name}, you are {int(age_hours)} hours old!!")
